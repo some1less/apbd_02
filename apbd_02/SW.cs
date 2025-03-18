1e07235df10fdf32f245c18be89e04095f5daff9
some1less <muracks.max@gmail.com>
2025-03-19 00:19:42 +0100

    Created and prepared child class Smartwatch

---
+++ apbd_02/SW.cs
@@ -0,0 +1,19 @@
+﻿namespace apbd_02;
+
+public class Smartwatch : Device
+{
+    
+    private int batteryLevel;
+    
+    public Smartwatch(int id, string name, bool isTurnedOn)
+        : base(id, name, isTurnedOn)
+    {
+        
+    }
+
+    public override void Activate()
+    {
+        
+    }
+    
+}
