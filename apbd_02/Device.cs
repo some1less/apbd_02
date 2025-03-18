1c94e8bff97823b6e6b1575116d91e7611d40519
some1less <muracks.max@gmail.com>
2025-03-19 00:01:14 +0100

    Implemented Device class

---
+++ apbd_02/Device.cs
@@ -0,0 +1,16 @@
+﻿namespace apbd_02;
+
+public class Device
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool isTurnedOn { get; set; }
+
+    
+    public void Launch()
+    {
+        Console.WriteLine(Id + " " + Name + " " + isTurnedOn);
+    }
+    
+    
+}
