6420e5d47c82b34ff2343d45c142370dffbe824a
some1less <muracks.max@gmail.com>
2025-03-19 23:58:20 +0100

    Updated ip address rule, network name and turnOn rule inside constructor, improved regex for ip address and implemented connect method

--- apbd_02/EmbeddedDevice.cs
+++ apbd_02/EmbeddedDevice.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using apbd_02.exception;
 
 namespace apbd_02;
 
@@ -11,6 +12,11 @@ public class EmbeddedDevice : Device
     {
         IpAddress = ipAddress;
         _networkName = networkName;
+        if (!_networkName.Contains("MD Ltd.") && isTurnedOn)
+        {
+            throw new ArgumentException("The network name must contain MD Ltd. to be turned on", "networkName");
+        }
+        Console.WriteLine("[Object] Embedded device created");
     }
 
     private string IpAddress
@@ -18,7 +24,7 @@ public class EmbeddedDevice : Device
         get {return _ipAddress;}
         set
         {
-            if (!isValidIpAddress(value))
+            if (!IsValidIpAddress(value))
             {
                 throw new ArgumentException("Invalid IP Address");
             }
@@ -29,11 +35,51 @@ public class EmbeddedDevice : Device
         }
     }
 
-    private bool isValidIpAddress(string ipAddress)
+    private static bool IsValidIpAddress(string ipAddress)
     {
-        string pattern = "/^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\b){4}$/gm";
+        string pattern = @"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$";
         Regex regex = new Regex(pattern);
-        
+
         return regex.IsMatch(ipAddress);
+    }
+
+    public override void TurnMode()
+    {
+        if (IsTurnedOn)
+        {
+            IsTurnedOn = false;
+            Console.WriteLine("[EmbeddedDevice] Turned Off");
+        }
+        else
+        {
+            if (Connect(_networkName))
+            {
+                IsTurnedOn = true;
+                Console.WriteLine("[EmbeddedDevice] Turned On");
+                Console.WriteLine("[EmbeddedDevice] Successfully connected Network Name: " + _networkName + " contains MD Ltd.");
+            }
+            else
+            {
+                throw new ConnectionException();
+            }
+            
+        }
+    }
+
+    private bool Connect(string networkName)
+    {
+        if (networkName.Contains("MD Ltd."))
+        {
+            return true;
+        }
+
+        return false;
+    }
+    
+    public override void Info()
+    {
+        Console.WriteLine($"Registered info about Embedded Device: \n" +
+                          $"ID: {Id}; Name: {Name}; TurnedOn: {IsTurnedOn}; IP address: {IpAddress}, " +
+                          $"Networking name: {_networkName}%");
     }
 }
